286da5fb063e33390d4cdc327bbd8ad2fbf16114
Rockey800 <150011633+Rockey800@users.noreply.github.com>
2025-04-01 10:11:30 +0300

    Update download_update_ngrok.ps1

--- download_update_ngrok.ps1
+++ download_update_ngrok.ps1
@@ -3,7 +3,14 @@ Write-Output "Checking for Ngrok executable..."
 # Define download URLs
 $ngrokDownloadUrl = "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip"
 $ngrokZipFile = "ngrok.zip"
-$ngrokExecutable = ".\ngrok.exe"
+$ngrokFolder = ".\ngrok"
+$ngrokExecutable = "$ngrokFolder\ngrok.exe"
+
+# Check if Ngrok is already downloaded
+if (Test-Path $ngrokExecutable) {
+    Write-Output "Ngrok is already installed."
+    exit 0
+}
 
 # Download Ngrok
 Write-Output "Downloading Ngrok..."
@@ -11,11 +18,15 @@ Invoke-WebRequest -Uri $ngrokDownloadUrl -OutFile $ngrokZipFile -ErrorAction Sto
 
 # Extract the Ngrok archive
 Write-Output "Extracting Ngrok..."
-Expand-Archive -Path $ngrokZipFile -DestinationPath .\ -Force
+Expand-Archive -Path $ngrokZipFile -DestinationPath $ngrokFolder -Force
+
+# Clean up the downloaded zip file
+Remove-Item -Path $ngrokZipFile -Force
 
 # Ensure the Ngrok executable exists
 if (-Not (Test-Path $ngrokExecutable)) {
     Write-Error "Ngrok download or extraction failed. Exiting..."
     exit 1
 }
+
 Write-Output "Ngrok downloaded successfully."
