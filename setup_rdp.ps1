55502e448a835ec1d8c7d564e53299c3bdcf99d1
Rockey800 <150011633+Rockey800@users.noreply.github.com>
2025-08-26 15:11:58 +0530

    Update and rename setup_rdp.ps1 to setup_guacamole.ps1

--- setup_rdp.ps1
+++
@@ -1,8 +0,0 @@
-# Enable RDP
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
-
-# Enable firewall rule
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-
-# Start RDP service
-Start-Service -Name TermService
