6efef68ccf243b96618a959e03b997b3231f1b6f
Rockey800 <150011633+Rockey800@users.noreply.github.com>
2025-08-26 14:35:14 +0530

    Update and rename download_update_ngrok.ps1 to download_update_cloudflared.ps1

---
+++ download_update_cloudflared.ps1
@@ -0,0 +1,9 @@
+$cloudDir = "C:\ProgramData\cloudflared"
+$cloudExe = "$cloudDir\cloudflared.exe"
+
+if (-Not (Test-Path $cloudExe)) {
+    Write-Host "🔍 Downloading cloudflared..."
+    New-Item -Path $cloudDir -ItemType Directory -Force | Out-Null
+
+    Invoke-WebRequest -Uri "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-amd64.exe" -OutFile $cloudExe
+}
