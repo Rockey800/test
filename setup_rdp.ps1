d7eef5dc9f924de4ee0d97aed0d1ee0be92d47bf
Rockey800 <150011633+Rockey800@users.noreply.github.com>
2025-08-26 14:39:31 +0530

    Update setup_rdp.ps1

--- setup_rdp.ps1
+++ setup_rdp.ps1
@@ -1,8 +1,8 @@
-# Enable RDP service
+# Enable RDP
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
 
 # Enable firewall rule
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
-# Start RDP service if not running
+# Start RDP service
 Start-Service -Name TermService
